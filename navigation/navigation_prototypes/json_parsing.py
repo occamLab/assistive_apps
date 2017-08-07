48610c683a8ea3ff797121aa78c142f7f1eb57d3
Jeremy <jeremychristopherryan@gmail.com>
2017-08-07 09:20:47 -0400

    Fixed error message if no args

--- navigation/navigation_prototypes/json_parsing.py
+++ navigation/navigation_prototypes/json_parsing.py
@@ -1,4 +1,5 @@
 import json
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -15,6 +16,16 @@ def get_navigation_positions(data):
     return (navigationDataList[:, 12],
         navigationDataList[:, 13],
         navigationDataList[:, 14])
+
+def get_navigation_times(data):
+    """ Returns a list of all times corresponding to the
+    navigation path positions. """
+
+    navigationTimeList = []
+    for time in data["navigationDataTime"]["L"]:
+        navigationTimeList.append(float(time[u'N']))
+    navigationTimeList = np.asarray(navigationTimeList)
+    return navigationTimeList
 
 def get_keypoint_positions(data):
     """ Reads json file and returns a tuple of three numpy arrays
@@ -61,8 +72,12 @@ def data_map_3d(data):
     nx, ny, nz = get_navigation_positions(data)
     px, py, pz = get_path_positions(data)
     kx, ky, kz = get_keypoint_positions(data)
+    points, instructions = get_instructions(data)
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
+    # for i, instruction in enumerate(instructions):
+    #     x, y, z = points[i-1]
+    #     ax.text(z, x, y + 2, str(i), 'x')
     ax.plot(nz, nx, ny)
     ax.plot(pz, px, py)
     ax.scatter(kz, kx, ky)
@@ -86,8 +101,55 @@ def iso_scale(xs, ys, zs):
         [avg_y - max_dev, avg_y + max_dev],
         [avg_z - max_dev, avg_z + max_dev])
 
+def nearest_nav_point(data, time):
+    """ Finds the index of the closest navigation point to the given time. """
+
+    nx, ny, nz = get_navigation_positions(data)
+    nt = get_navigation_times(data)
+    dif = abs(nt - time)
+    mdif_index, mdif = min(enumerate(dif), key=lambda f:f[1])
+    return mdif_index
+
+def get_navigation_point(data, index):
+    """ Returns the point of the given index from the navigation path. """
+
+    nx, ny, nz = get_navigation_positions(data)
+    return (nx[index], ny[index], nz[index])
+
+def get_instructions(data):
+    """ Gets a list of indexes for points along the path instructions were
+    given, and a list of all voice instructions for that path. """
+
+    point_indexes = [nearest_nav_point(data, time) \
+        for time in get_instruction_times(data)]
+    points = [get_navigation_point(data, index) for index in point_indexes]
+    instructions = get_voice_text(data)
+    return points, instructions
+
+def get_voice_text(data):
+    """ Get a list of all voice instructions given for that run. """
+
+    voiceDataList = []
+    for string in data["speechData"]["L"]:
+        voiceDataList.append(str(string[u'S']))
+    voiceDataList = np.asarray(voiceDataList)
+    return voiceDataList
+
+def get_instruction_times(data):
+    """ Get a list of times that voice instructions were given. """
+
+    instructionTimeList = []
+    for time in data["speechDataTime"]["L"]:
+        instructionTimeList.append(float(time[u'N']))
+    instrucitonTimeList = np.asarray(instructionTimeList)
+    return instructionTimeList
+
 if __name__ == '__main__':
-    with open('data.json') as data_file:
-        data = json.load(data_file)
-    data_map_birdseye(data)
-    data_map_3d(data)
+    if len(sys.argv) <= 1:
+        print("No data file selected. Use the following syntax:")
+        print("python json_parsing.py data.json")
+    else:
+        with open(sys.argv[1]) as data_file:
+            data = json.load(data_file)
+        #data_map_birdseye(data)
+        data_map_3d(data)
